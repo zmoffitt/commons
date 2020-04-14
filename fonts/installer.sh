9c96a4746e1a1395bcc7e2df7257674b17dbc14d
Zachary Moffitt <13591008+zmoffitt@users.noreply.github.com>
2020-04-14 13:53:01 -0400

    This is a combination of 3 commits.

--- fonts/installer.sh
+++ fonts/installer.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-#find . -type f -name "*.pkg" -execdir sh -c "sudo installer -pkg '{}' -target /;" \;
+# ===========
+PASS='\033[0;32m ✔\033[0m'
+FAIL='\033[0;31m ✘\033[0m'
+BLUE='\033[0;34m'
+CLEAR='\033[0m'
+# ============
 
-PATH=$(pwd)
-FONTS=$(find .$PATH -type f -name "*.pkg")
-
-for font in ${FONTS}
-do
-    echo $fone
-done
+echo -e "\n$BLUE San Francisco and New York font installer $CLEAR"
+echo
+while IFS=  read -r -d $'\0'; do
+    sudo installer -store -pkg "$REPLY" -target / > /dev/null 2>&1
+    THIS=$(echo ${REPLY##*/} | sed 's/.pkg$//g')
+    [[ $? -eq 0 ]] && echo -e $PASS $THIS || echo -e $FAIL $THIS
+done < <(find $(pwd) -type f -name "*.pkg" -print0)
